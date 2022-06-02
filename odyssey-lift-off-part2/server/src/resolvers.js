95fb62f7154500d71054626303641ceacc869898
Gagan <g.deepsingh1@gmail.com>
2022-06-02 13:24:40 -0600

    mutation graphs

--- odyssey-lift-off-part2/server/src/resolvers.js
+++ odyssey-lift-off-part2/server/src/resolvers.js
@@ -4,16 +4,37 @@ const resolvers = {
       return dataSources.trackAPI.getTracksForHome();
     },
     track: (_, { id }, { dataSources }) => {
-        return dataSources.trackAPI.getTrack(id);
-    }
+      return dataSources.trackAPI.getTrack(id);
+    },
+  },
+  Mutation: {
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully increment views for track ${id}`,
+          track,
+        };
+      } catch (error) {
+        return {
+          code: error.extensions.response.status,
+          success: false,
+          message: error.extensions.response.body,
+          track: null
+        };
+      }
+    },
   },
   Track: {
     author: (parent, args, { dataSources }, info) => {
-        return dataSources.trackAPI.getAuthor(parent.authorId);
+      return dataSources.trackAPI.getAuthor(parent.authorId);
     },
     modules: ({ id }, _, { dataSources }) => {
-        return dataSources.trackAPI.getTrackModules(id);
-    }
+      return dataSources.trackAPI.getTrackModules(id);
+    },
   },
 };
 
