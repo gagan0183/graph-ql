2ead78ee49cefe63aeee4347efa5099d9ba2de00
Gagan <g.deepsingh1@gmail.com>
2022-06-01 18:10:27 -0600

    clients calling graphs apis app

--- odyssey-lift-off-part2/client/src/containers/track-card.js
+++ odyssey-lift-off-part2/client/src/containers/track-card.js
@@ -2,22 +2,23 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { colors, mq } from '../styles';
 import { humanReadableTimeFromSeconds } from '../utils/helpers';
+import { Link } from "@reach/router";
 
 /**
  * Track Card component renders basic info in a card format
  * for each track populating the tracks grid homepage.
  */
 const TrackCard = ({ track }) => {
-  const { title, thumbnail, author, length, modulesCount } = track;
+  const { title, thumbnail, author, length, modulesCount, id } = track;
 
   return (
-    <CardContainer>
+    <CardContainer to={`track/${id}`}>
       <CardContent>
         <CardImageContainer>
           <CardImage src={thumbnail} alt={title} />
         </CardImageContainer>
         <CardBody>
-          <CardTitle>{title || ''}</CardTitle>
+          <CardTitle>{title || ""}</CardTitle>
           <CardFooter>
             <AuthorImage src={author.photo} />
             <AuthorAndTrack>
@@ -36,7 +37,7 @@ const TrackCard = ({ track }) => {
 export default TrackCard;
 
 /** Track Card styled components */
-const CardContainer = styled.div({
+const CardContainer = styled(Link)({
   borderRadius: 6,
   color: colors.text,
   backgroundSize: 'cover',
